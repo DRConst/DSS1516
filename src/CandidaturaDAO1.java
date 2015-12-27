c7891b06af44ce46cb2aa2ed49400675dc3a3a83
hokuto22 <hokuto2015@gmail.com>
2015-12-27 20:55:32 +0000

    Rename CandidaturaDAO.java to CandidaturaDAO1.java

---
+++ src/CandidaturaDAO1.java
@@ -0,0 +1,243 @@
+package DAOs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
+public class CandidaturaDAO implements Map<String,Candidatura> {
+    
+    public Connection conn;
+    
+    public void clear () {       
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM candidatura");
+            ps.executeUpdate();
+        }catch (SQLException | ClassNotFoundException e) { 
+        e.printStackTrace(); 
+        } finally { 
+            try {
+                conn.close();
+                conn = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public boolean containsKey(Object key) throws NullPointerException {
+        boolean c = false;
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("Select nome from candidatura where nome = '" + (String)key + "'" );
+            ResultSet rs = ps.executeQuery();
+            c = rs.next();
+        } catch (SQLException | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        }
+        return c;
+    }       
+    
+    public boolean containsValue(Object value) {
+        throw new NullPointerException("public boolean containsValue(Object value) not implemented!");
+    }
+    
+    public Set<Map.Entry<String,Candidatura>> entrySet() {
+        throw new NullPointerException("public Set<Map.Entry<String,Equipa>> entrySet() not implemented!");
+    }
+    
+    public boolean equals(Object o) {
+        throw new NullPointerException("public boolean equals(Object o) not implemented!");
+    }
+    
+    public Candidatura get(Object key) {
+        Candidatura c = null;
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("Select * from candidato where nome'" +(String)key +"'");
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                Candidatura ca = new Candidatura(rs.getInt("Candidatura_id"),rs.getString("Nome"),rs.getDate("Data"),rs.getString("Eleicao"));
+                c.add(ca);
+              }
+        } catch (SQLException  | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return c;
+    }
+    
+    public int hashCode() {
+        return this.conn.hashCode();
+    }
+    
+    public boolean isEmpty() {
+        boolean b = false;
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM candidatura");
+            ResultSet rs = ps.executeQuery();
+            b = !rs.next();
+        } catch (SQLException  | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        } 
+        return b;
+    }
+    
+    public Set<String> keySet() {
+        Set<String> res = new HashSet<String>();
+        
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("SELECT nome FROM candidatura");
+            ResultSet rs = ps.executeQuery();
+            for (;rs.next();){
+                res.add(rs.getString("nome"));
+            }
+	} catch (SQLException  | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        }
+        return res;
+    }
+    
+    public Candidatura put(String key, Candidatura value) {
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM candidatura WHERE nome='"+key+"'");
+            ps.executeUpdate();
+            PreparedStatement ps1 = conn.prepareStatement("Select BI from candidato where nome = '"+value.getCandidato()+"'" );
+            ResultSet rs = ps1.executeQuery();
+            PreparedStatement sql =conn.prepareStatement("INSERT INTO  VALUES ('"+key+"','"+value.getNome()+"','"+value.getData()+"','"+value.getEleicao()+"','"+rs.getInt("BI")+"')");
+            sql.executeUpdate();
+           } catch (SQLException  | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        }
+         return new Candidatura(key,value.getNome(),value.getData(),value.getEleicao(),value.getCandidato);
+    }
+
+    public void putAll(Map<? extends String,? extends Candidatura> t) {
+        throw new NullPointerException("Not implemented!");
+    }
+    
+    public Candidatura remove(Object key) {
+        throw new NullPointerException("public AssembleiaDeVoto remove(Object key)  not implemented!");
+    }
+    
+    public void remove(String nome) {
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM candidatura WHERE Nome =' "+nome+"'");
+            ps.executeUpdate();
+        } catch (SQLException  | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        }}
+    
+    public int size() {
+        int i = -1;
+        try {
+            conn = SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("Select count(id) from candidatura");
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                i=rs.getInt(1);
+            }
+        } catch (SQLException | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+        }
+        return i;
+    }
+             
+    public Collection<Candidatura> values() {
+        ArrayList<Candidatura> ct = new ArrayList<Candidatura>();
+       
+        try { 
+            conn =SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM candidatura");
+            ResultSet rs = ps.executeQuery();
+            for (;rs.next();) {
+            	Candidatura ca = new Candidatura(rs.getString("Nome"),rs.getDate("Data"),rs.getString("Eleicao"));
+                ct.add(ca);
+                }
+        } catch (SQLException  | ClassNotFoundException e) { 
+            e.printStackTrace(); 
+        } finally { 
+            try { 
+                conn.close();     
+            } catch (Exception e) { 
+                e.printStackTrace(); 
+            } 
+      }
+        return ct;
+    } 
+    
+    public ArrayList<String> getCandidaturas(String eleicao) throws SQLException{
+    	ArrayList<String> res = new ArrayList<String>();
+    	try{
+            conn =SqlConnect.connect();
+            PreparedStatement ps = conn.prepareStatement("Select * from candidatura where eleicao ='" + eleicao +"'");
+            ResultSet rs = ps.executeQuery();
+            for (;rs.next();){
+                res.add(rs.getString("Nome"));
+            }
+        }catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                conn.close();
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return res;
+    }
+    
+}
+
